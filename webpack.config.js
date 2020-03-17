03ffcf5095a4573f86c43891e1c2ce5ddb39ef2c
brodao <brodao@gmail.com>
2020-03-17 14:31:14 -0300

    Reestruturação da extensão para transferência do monitor para extensão própria.

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,7 @@ module.exports = {
   //O webpack, pega todos os fontes tsx e os compacta em um unico arquivo .js. Isso é feito para contornar algumas limitações e alguns browsers que não aceitam a instrução import.
   //O entry pode ser definido com um objeto. A chave, ou no nome da propriedade, nesse caso sera o nome de saida do arquivo.
   entry: {
-    timeLineView: "./src/debug/tdsreplay/app/index.tsx",
-    monitorView: "./src/monitor/app/index.tsx"
+    timeLineView: "./src/debug/tdsreplay/app/index.tsx"
   },
   output: {
     //Todos os arquivos tsx serão compilados e gerados seus equivalentes js na mesma pasta
