ca6fa34eb27fdd40b44e0139529e19f663c4a86f
lwtnb-wrk <leo.watanabe@totvs.com.br>
2020-12-02 15:02:23 -0300

    ajuste vscodepath

--- src/utils.ts
+++ src/utils.ts
@@ -108,13 +108,11 @@ export default class Utils {
    * Retorna o path completo do launch.json
    */
   static getLaunchConfigFile() {
-    let rootPath: string = vscode.workspace.rootPath || process.cwd();
-
-    return path.join(rootPath, ".vscode", "launch.json");
+    return path.join(this.getVSCodePath(), "launch.json");
   }
 
   /**
-   * Retorna o path da pastar .vscode dentro do workspace
+   * Retorna o path da pasta .vscode dentro do workspace
    */
   static getVSCodePath() {
     let rootPath: string = vscode.workspace.rootPath || process.cwd();
