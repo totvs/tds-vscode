b2b95ab54849d24ad4c5fd979edd520a006f097a
Alan Cândido <brodao@gmail.com>
2021-10-07 12:46:43 -0300

    Implementação de testes de operações em RPO

--- test/e2e/server-view/rpo-operations-test.ts
+++ test/e2e/server-view/rpo-operations-test.ts
@@ -2,59 +2,69 @@
 import { expect } from "chai";
 import { describe, before, it } from "mocha";
 import {
-  Workbench,
-  TreeItem,
-  ViewItemAction,
-  InputBox,
-  QuickPickItem,
+  Notification
 } from "vscode-extension-tester";
 import {
   delay,
   openAdvplProject,
-  takeQuickPickAction
+  waitNotification
 } from "../helper";
+import { RpoPageObject } from "../page-objects/rpo-po";
 import { ServerTreeItemPageObject } from "../page-objects/server-tree-item-po";
 import { ServerTreePageObject } from "../page-objects/server-tree-po";
 import { StatusPageObject } from "../page-objects/status-po";
 import { ADMIN_USER_DATA, LOCALHOST_DATA } from "../servers-data";
 
 // Create a Mocha suite
-describe("RPO Operations", () => {
+describe.skip("RPO Operations", () => {
   let serverTreePO: ServerTreePageObject;
   let serverItemPO: ServerTreeItemPageObject;
-  let pickBox: InputBox;
-  let title: string = "";
   let statusBarPO: StatusPageObject;
 
   const LOCALHOST_NAME: string = LOCALHOST_DATA.serverName;
   const LOCALHOST_ENVIRONMENT: string = LOCALHOST_DATA.environment;
 
   before(async () => {
+    console.error("before.1");
     await openAdvplProject();
-    await delay(2000);
+    console.error("before.2");
 
     serverTreePO = new ServerTreePageObject();
     serverTreePO.openView();
-    await delay();
+    console.error("before.3");
 
     await serverTreePO.addNewServer(LOCALHOST_DATA);
+    console.error("before.4");
 
     statusBarPO = new StatusPageObject();
+    console.error("before.5");
 
     await delay();
+    console.error("before.8");
   });
 
   beforeEach(async () => {
+    console.error("beforeEach.1");
     await serverTreePO.connect(LOCALHOST_NAME, LOCALHOST_ENVIRONMENT, ADMIN_USER_DATA);
+    console.error("beforeEach.2");
     serverItemPO = new ServerTreeItemPageObject(await serverTreePO.getServerTreeItem(LOCALHOST_NAME));
+    console.error("beforeEach.3");
   })
 
   afterEach(async () => {
+    console.error("afterEach.1");
+
     await serverItemPO.fireDisconnectAction();
-    serverItemPO = null;
+    console.error("afterEach.2");
   })
 
-  it.skip("Check Integraty", async () => {
+  it("Check Integrity", async () => {
+    await serverItemPO.fireCheckIntegrity();
+
+    expect(await statusBarPO.waitCheckIntegrity()).is.true;
+
+    const notification: Notification = await waitNotification("RPO intact.");
+    expect(notification).not.is.undefined;
   });
 
   it.skip("Revalidate", async () => {
