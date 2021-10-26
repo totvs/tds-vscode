c54fb6311ea6af8ac7d178aa45ba546615aff9dd
Alan Cândido <brodao@gmail.com>
2021-10-26 16:36:55 -0300

    Reversão do PO
    O uso de async/await em cascata gerou comportamento inesperados.

--- test/e2e/page-objects/abstract-po.ts
+++ test/e2e/page-objects/abstract-po.ts
@@ -1,96 +1,37 @@
-import { By, EditorView, TextEditor, WebElement, WebView } from "vscode-extension-tester"
-import { delay } from "../helper"
-import { expect } from "chai"
+import {
+  By,
+  EditorView,
+  TextEditor,
+  WebElement,
+  WebView,
+} from "vscode-extension-tester";
+import { delay } from "../helper";
+import { expect } from "chai";
 
 export class AbstractPageObject {
-	private _webView: WebView;
+  private _webView: WebView;
 
-	public get webView (): WebView {
-	  return this._webView
-	}
-
-	async beginWebView (): Promise<void> {
-	  this._webView = new WebView()
-
-	  await this.webView.wait()
-	  await this.webView.switchToFrame()
-	  await delay(2000)
-	}
-
-	async endWebView (): Promise<void> {
-	  this.webView.switchBack()
-	  await delay(2000)
-
-	  this._webView = null
-	}
-
-	async findElement (id: string) {
-	  return await this.webView.findWebElement(By.id(id))
-	}
-
-	async getValue (elementId: string): Promise<string> {
-	  const element: WebElement = await this.findElement(elementId)
-
-	  return await element.getAttribute("value") || await element.getText()
-	}
-
-	async setValue (elementId: string, value: string): Promise<void> {
-	  const element: WebElement = await this.findElement(elementId)
-	  await element.clear()
-	  await element.sendKeys(value)
-	}
-
-	async click (elementId: string): Promise<void> {
-	  const element: WebElement = await this.findElement(elementId)
-
-	  await element.click()
-	  await delay()
-	}
-}
-
-export class AbstractEditorPageObject {
-  private _view: EditorView;
-  private _editor: TextEditor;
-  private _expectTitle: string;
-
-  constructor(expectTitle: string) {
-    this._expectTitle = expectTitle;
+  public get webView(): WebView {
+    return this._webView;
   }
 
-  public get expectTitle(): string {
-    return this._expectTitle;
+  async beginWebView(): Promise<void> {
+    this._webView = new WebView();
+
+    await this.webView.wait();
+    await this.webView.switchToFrame();
+    await delay(2000);
   }
 
-  public get view(): EditorView {
-    if (!this._view) {
-      this._view = new EditorView();
-    }
+  async endWebView(): Promise<void> {
+    this.webView.switchBack();
+    await delay(2000);
 
-    return this._view;
-  }
-
-  public get editor(): TextEditor {
-    if (!this._editor) {
-      this._editor = new TextEditor(this.view);
-    }
-
-    return this._editor;
-  }
-
-  async close() {
-    await delay();
-    await this.view.closeEditor(await this.editor.getTitle());
-  }
-
-  async isOpen(): Promise<boolean> {
-    await delay();
-    const title: string = await this.editor.getTitle();
-
-    return title == this.expectTitle;
+    this._webView = null;
   }
 
   async findElement(id: string) {
-    return await this.editor.findElement(By.id(id));
+    return await this.webView.findWebElement(By.id(id));
   }
 
   async getValue(elementId: string): Promise<string> {
@@ -103,5 +44,12 @@ export class AbstractEditorPageObject {
     const element: WebElement = await this.findElement(elementId);
     await element.clear();
     await element.sendKeys(value);
+  }
+
+  async click(elementId: string): Promise<void> {
+    const element: WebElement = await this.findElement(elementId);
+
+    await element.click();
+    await delay();
   }
 }
