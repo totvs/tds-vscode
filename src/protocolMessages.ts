1b7026e0fe250995640a580461a97018fb9ee302
brodao <brodao@gmail.com>
2023-05-16 08:11:17 -0300

    Squashed commit of the following:
    commit 9ae2153e99fca7d54f8c1857149c44e919c72ec0
    Author: DanielYampolschi <daniel.yampolschi@totvs.com.br>
    Date:   Tue May 9 10:37:04 2023 -0300
    
        Atualizado versao do plugin
    
    commit 017977b9077dc8e7d48cee8dae0e548a8d7dadc6
    Author: DanielYampolschi <daniel.yampolschi@totvs.com.br>
    Date:   Tue May 9 09:51:22 2023 -0300
    
        Subido versao do LS e DA
    
    commit 3593fb6467d5a059e4154c53694c72ca9f699e69
    Author: DanielYampolschi <daniel.yampolschi@totvs.com.br>
    Date:   Fri Apr 28 15:35:33 2023 -0300
    
        Alteardo versao para 1.3.18-RC3
    
    commit 785416a13eb009b5f19a914e8d319d658cc19f0c
    Merge: b790e133 adbdb8c3
    Author: DanielYampolschi <daniel.yampolschi@totvs.com.br>
    Date:   Fri Apr 28 15:33:43 2023 -0300
    
        Merge branch 'dev' of https://github.com/totvs/tds-vscode into dev
    
    commit b790e133bc593ae79c1ba9e8fa54753f8e130c2d
    Author: DanielYampolschi <daniel.yampolschi@totvs.com.br>
    Date:   Fri Apr 28 15:28:42 2023 -0300
    
        Alteado a msgm validation com o tipo de servidor
    
    commit 7eb92b84f9896364186cbdf654652663ffc4a6a8
    Author: DanielYampolschi <daniel.yampolschi@totvs.com.br>
    Date:   Fri Apr 28 15:27:03 2023 -0300
    
        Atualizado a versão minima do ADVPLS para 1.2.63
    
    commit adbdb8c3509649d2de0278b00b2d7e19ae52d53f
    Merge: 5c3e48e8 3707b6d4
    Author: lwtnb-wrk <leo.watanabe@totvs.com.br>
    Date:   Thu Apr 20 17:23:51 2023 -0300
    
        Merge branch 'dev' of https://github.com/totvs/tds-vscode into dev
    
    commit 5c3e48e8fd89d5fa149bc71564b63a6029ebec5c
    Author: lwtnb-wrk <leo.watanabe@totvs.com.br>
    Date:   Thu Apr 20 17:23:46 2023 -0300
    
        bump versions

--- src/protocolMessages.ts
+++ src/protocolMessages.ts
@@ -238,7 +238,8 @@ export function sendReconnectRequest(
 
 export function sendValidationRequest(
   addres: string,
-  port: number
+  port: number,
+  serverType: string
 ): Thenable<IValidationInfo> {
   if (typeof port !== "number") {
     port = parseInt(port);
@@ -249,6 +250,7 @@ export function sendValidationRequest(
       validationInfo: {
         server: addres,
         port: port,
+        serverType: serverType
       },
     })
     .then(
