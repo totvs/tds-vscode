c9baa08689455de8da0649bbb2ad4621392efc80
brodao <brodao@gmail.com>
2023-07-20 11:03:44 -0300

    Update xreferences.ts
    Ajuste na apresentação dos resultados.

--- src/xreferences.ts
+++ src/xreferences.ts
@@ -6,30 +6,23 @@ export function registerXRef(context: vscode.ExtensionContext) {
 		vscode.commands.registerCommand("advpl.showXrefs", showXrefsHandlerCmd));
 }
 
-function clearUri(uri: vscode.Uri): string {
-	let aux = uri.toString();
-
-	if (aux.startsWith("file:///")) { //windows é com ///!?
-		aux = aux.substring(8);
-	} else if (aux.startsWith("file://")) {
-		aux = aux.substring(7);
-	}
-	aux = aux.replace("%3A", ":");
-
-	return aux;
-}
-
 function convertRange(range: lsp.Range): vscode.Range {
 	return new vscode.Range(range.start.line, range.start.character, range.end.line, range.end.character);
 }
 
-function showXrefsHandlerCmd(uri: vscode.Uri, position: vscode.Position, xrefArgs: any[]) {
-	const locations = (xrefArgs).map(loc => {
-		return new vscode.Location(vscode.Uri.parse(loc.uri), convertRange(loc.range));
-	});
+function showXrefsHandlerCmd(kind: string, uri: vscode.Uri, position: vscode.Position, xrefArgs: any[]) {
+	var locations: any[] = [];
 
-	vscode.commands.executeCommand(
-		'editor.action.showReferences',
-		vscode.Uri.parse(uri.toString()), position,
-		locations);
+	if (kind == "function") {
+		locations = (xrefArgs).map(loc => {
+			return new vscode.Location(vscode.Uri.parse(loc.uri), convertRange(loc.range));
+		});
+	}
+
+	if (locations.length > 0) {
+		vscode.commands.executeCommand(
+			'editor.action.showReferences',
+			vscode.Uri.parse(uri.toString()), position,
+			locations);
+	}
 }
