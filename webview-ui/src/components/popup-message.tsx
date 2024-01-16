86ea934b9a45b7051954ff4edff7a889c74ce108
Alan Candido <brodao@gmail.com>
2024-01-16 14:26:18 -0300

    Posicionamento do popup de mensagem
    Ajustado o posicionamento do popup em relação ao elemento, ficando similar ao apresentado pelo VSCode na página 'settings'.

--- webview-ui/src/components/popup-message.tsx
+++ webview-ui/src/components/popup-message.tsx
@@ -7,11 +7,31 @@ export interface IPopupMessage {
 }
 
 export default function PopupMessage(props: IPopupMessage): JSX.Element {
+	const OFFSET_LEFT: number = 20;
+	const OFFSET_TOP: number = 2;
+
 	const type = props.type || "info";
+	var activateClick: boolean = false;
+
+	const prepareElement: any = (event: any): HTMLSpanElement => {
+		var popup = document.getElementById("popup_" + props.fieldName) as HTMLElement;
+		var parent = popup.parentElement!.parentElement as HTMLElement;
+
+		if (parent) {
+			popup.style.width = `${parent.clientWidth - OFFSET_LEFT}px`;
+			popup.style.left = `${OFFSET_LEFT - parent.clientWidth}px`;
+			popup.style.top = `${parent.offsetHeight - OFFSET_TOP}px`;
+		}
+
+		return popup;
+	};
 
 	const mouseOver: any = (event: any) => {
-		var popup = document.getElementById("popup_" + props.fieldName) as HTMLElement;
-		popup.classList.toggle("show");
+		if (!activateClick) {
+			const popup = prepareElement(event);
+
+			popup.classList.toggle("show");
+		}
 	};
 
 	const mouseOut: any = (event: any) => {
@@ -19,6 +39,14 @@ export default function PopupMessage(props: IPopupMessage): JSX.Element {
 		popup.classList.toggle("show");
 	};
 
+	const mouseClick: any = (event: any) => {
+		const popup = prepareElement(event);
+
+		popup.classList.toggle("show");
+		activateClick = !activateClick;
+	};
+
+	//onClick={(event) => mouseClick(event)}
 	return (
 		<span slot="end" className={`tds-popup codicon codicon-${type} tds-${type}`}
 			onMouseOver={(event) => mouseOver(event)}
