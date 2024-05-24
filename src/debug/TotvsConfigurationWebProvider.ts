90fdc14148afd997f6ae9daa6af015e2b4aa0a39
Alan Candido <brodao@gmail.com>
2024-05-24 18:41:17 -0300

    Implementação de argumentos de linha de comando
    Implementação de argumentos de linha de comando para o navegador a ser utilizado na depuração WEB.
    Chamado GIT: https://github.com/totvs/tds-vscode/issues/1233

--- src/debug/TotvsConfigurationWebProvider.ts
+++ src/debug/TotvsConfigurationWebProvider.ts
@@ -4,8 +4,7 @@ import { TotvsConfigurationProvider } from "./TotvsConfigurationProvider";
 
 export class TotvsConfigurationWebProvider
   extends TotvsConfigurationProvider
-  implements DebugConfigurationProvider
-{
+  implements DebugConfigurationProvider {
   static _TYPE: string = "totvs_language_web_debug";
   static _NAME: string = "TOTVS Language Web Debug (SmartClient HTML)";
   static _SC_BIN: string = "http://localhost:8080";
@@ -19,16 +18,19 @@ export class TotvsConfigurationWebProvider
 
   protected finalize(config: DebugConfiguration) {
     const cfg = vscode.workspace.getConfiguration("totvsLanguageServer");
-    const webNavigator: string | undefined = cfg.get("web.navigator");
+    const webNavigator: string = cfg.get("web.navigator") || "";
+    const webNavigatorArgs: string[] = cfg.get("web.arguments") || [];
 
-    if (!webNavigator || webNavigator === "") {
+    if (webNavigator === "") {
       window.showErrorMessage(
         vscode.l10n.t("Parameter WebNavigator not informed.")
       );
+
       return undefined; // abort launch
     }
 
     config.webNavigator = webNavigator;
+    config.webNavigatorArgs = webNavigatorArgs;
 
     return config;
   }
